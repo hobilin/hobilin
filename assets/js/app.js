773d3c92f9ea91e0bf569a62024dba25abf5ffb5
hobilin <paulibareyes@gmail.com>
2018-02-19 05:33:28 -0300

    maqueta

---
+++ assets/js/app.js
@@ -0,0 +1,25 @@
+ $(document).ready(function(){
+      $('.carousel').carousel();
+    });
+        
+
+/*
+var image = document.getElementById('texture');
+var aquarelle = new Aquarelle(image, 'assets/img/mask.png', {
+  'fromAmplitude': '50',
+  'toAmplitude': '0',
+  'fromFrequency': '8',
+  'toFrequency': '7',
+  'fromOffset': '-30',
+  'toOffset': '28',
+  'autoplay': 'false',
+  'loop': 'true',
+
+});
+
+ aquarelle.addEventListener('created', function() {
+        var canvas = this.getCanvas();
+        canvas.removeAttribute('style');
+        image.parentNode.insertBefore(canvas, image.nextSibling);
+        image.parentNode.removeChild(image);
+    });*/
