21662c3400d84ed4c88066a6310fcf94f2a90088
hobilin <paulibareyes@gmail.com>
2018-03-04 22:38:04 -0300

    añadiendo funcionalidad

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,25 +1,103 @@
  $(document).ready(function(){
       $('.carousel').carousel();
+
+  
+      $('.myself').hide();
+      $('.projects').hide();
+      $('.contact').hide();
+
+      $('#code').click(function() {
+        $('.tip').hide();
+        $(this).removeClass('dot');
+        $(this).addClass('flower-dot');
+        $(this).siblings().removeClass('flower-dot');
+        $(this).siblings().addClass('dot');
+        $(this).children().attr('src', 'assets/img/flower-dot.png');
+        $(this).siblings().children().attr('src', 'assets/img/dot.png')
+
+        $('.main').show();
+        $('.projects').hide();
+        $('.contact').hide();
+        $('.myself').hide();
+      })
+
+      $('#skills').click(function() {
+        $('.tip').hide();
+        $(this).removeClass('dot');
+        $(this).addClass('flower-dot');
+        $(this).siblings().removeClass('flower-dot');
+        $(this).siblings().addClass('dot');
+        $(this).children().attr('src', 'assets/img/flower-dot.png');
+        $(this).siblings().children().attr('src', 'assets/img/dot.png')
+
+        $('.main').hide();
+        $('.projects').hide();
+        $('.contact').hide();
+        $('.myself').show();
+
+      });
+
+      $('#projects').click(function() {
+        $('.tip').hide();
+        $(this).removeClass('dot');
+        $(this).addClass('flower-dot');
+        $(this).siblings().removeClass('flower-dot');
+        $(this).siblings().addClass('dot');
+        $(this).children().attr('src', 'assets/img/flower-dot.png');
+        $(this).siblings().children().attr('src', 'assets/img/dot.png');
+
+        $('.main').hide();
+        $('.projects').show();
+        $('.contact').hide();
+        $('.myself').hide();
+      });
+
+      $('#contact').click(function() {
+        $('.tip').hide();
+        $(this).removeClass('dot');
+        $(this).addClass('flower-dot');
+        $(this).siblings().removeClass('flower-dot');
+        $(this).siblings().addClass('dot');
+        $(this).children().attr('src', 'assets/img/flower-dot.png');
+        $(this).siblings().children().attr('src', 'assets/img/dot.png')
+
+        $('.main').hide();
+        $('.projects').hide();
+        $('.contact').show();
+        $('.myself').hide();
+      });
+
+
+
     });
         
 
-/*
-var image = document.getElementById('texture');
-var aquarelle = new Aquarelle(image, 'assets/img/mask.png', {
-  'fromAmplitude': '50',
-  'toAmplitude': '0',
-  'fromFrequency': '8',
-  'toFrequency': '7',
-  'fromOffset': '-30',
-  'toOffset': '28',
-  'autoplay': 'false',
-  'loop': 'true',
 
-});
 
- aquarelle.addEventListener('created', function() {
+    var fade = document.querySelector('.fade');
+
+
+    var image = document.getElementById('splash');
+    var aquarelle = new Aquarelle(image, 'assets/img/mask.png', {
+        autoplay: true,
+        loop: true,
+    });
+
+    aquarelle.addEventListener('created', function() {
         var canvas = this.getCanvas();
+        console.log(canvas)
         canvas.removeAttribute('style');
         image.parentNode.insertBefore(canvas, image.nextSibling);
         image.parentNode.removeChild(image);
-    });*/
+    });
+
+    aquarelle.addEventListener('changed', function(event) {
+
+
+        var canvas = this.getCanvas();
+        canvas.style.webkitTransform = canvas.style.transform = 'translate(-40%, -50%)';
+
+
+
+    });
+
